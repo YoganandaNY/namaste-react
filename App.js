76c593087729f9ba500861a86e8b70f19a6729e6
YoganandaNY <136065256+YoganandaNY@users.noreply.github.com>
2023-06-10 18:01:56 +0530

    episode-02

--- App.js
+++ App.js
@@ -1,44 +1,18 @@
-/**
- * <div id="parent">
- *  <div id="child">
- *      <h1>I am h1 Tag</h1>
- *      <h2>I am h2 Tag</h2>
- *  </div>
- *  <div id="child2">
- *      <h1>I am h1 Tag</h1>
- *      <h2>I am h2 Tag</h2>
- *  </div>
- * </div>
- * 
- * ReactElement(object) ==> HTML(Browser Understands)
- */
-
-// const heading = React.createElement(
-//     "h1",
-//     { id: "heading", xyz: "abc"}, 
-//     "Hello World from React!"
-//  );
-
-//  console.log(heading)  //Object
+import React from "react";
+import ReactDOM  from "react-dom/client";
 
 const parent = React.createElement("div", {id:"parent"}, 
-///   React.createElement("div", {id:"child"},
      [
         React.createElement("div", {id:"child"},[
-        React.createElement("h1", {}, "I am h1 Tag"), 
-        React.createElement( "h2", {}, "I am h2 Tag")
+        React.createElement("h1", {}, "This is Namaste React Web Series"), 
+        React.createElement( "h2", {}, "By Yogananda")
     ]),
         React.createElement("div", {id:"child2"},[
         React.createElement("h1", {}, "I am h1 Tag"), 
         React.createElement( "h2", {}, "I am h2 Tag")])
-     ]
-//   React.createElement("h1", {}, "I am h1 Tag")
-///    [React.createElement("h1", {}, "I am h1 Tag"), React.createElement( "h2", {}, "I am h2 Tag")] 
-    );
+     ]);
 
-// JSX    
-
-    console.log(parent)  //object
+console.log(parent)  //object
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
--- README.md
+++ README.md
@@ -1,1 +1,23 @@
-# Namaste React EP - 01
+# Namaste React 
+
+# https://github.com/YoganandaNY/namaste-react
+
+
+# Parcel
+- Dev Build
+- Local Server
+- HMR = Hot Module Replacement
+- File Watching Algorithm - written in C++
+- Caching - Faster Builds
+- Image Optimization
+- Minification
+- Bundling
+- Compress
+- Consistant Hashing
+- Code Splitting
+- Differential Bundling - Support older browsers
+- Diagnostic
+- Error Handling
+- HTTPs
+- Tree Shaking - remove unused code
+- Different dev and prod bundles
